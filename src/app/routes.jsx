efae190d0c895788cf84cfcf3cbcbb7d603aadec
Hasinjo-M <118592092+Hasinjo-M@users.noreply.github.com>
2024-01-24 00:26:19 -0800

    Changement

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -55,7 +55,7 @@ const routes = [
   { path: '/session/signin', element: <JwtLogin /> },
   { path: '/session/signup', element: <JwtRegister /> },
 
-  { path: '/', element: <Navigate to="/accueil" /> },
+  { path: '/', element: <Navigate to="/categories" /> },
   { path: '*', element: <NotFound /> }
 ];
 
