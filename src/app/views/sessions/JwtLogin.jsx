87bffbff15731d53f5e6bd7e1ed89ac6dd91a10c
Hasinjo-M <118592092+Hasinjo-M@users.noreply.github.com>
2024-01-24 10:34:18 -0800

    very

--- src/app/views/sessions/JwtLogin.jsx
+++ src/app/views/sessions/JwtLogin.jsx
@@ -69,7 +69,7 @@ const JwtLogin = () => {
       if (response.ok) {
         console.log(responseData);
         localStorage.setItem('token', responseData.data);
-        window.location.href = 'https://main--stirring-chebakia-f56620.netlify.app/accueil';
+        window.location.href = 'accueil';
       } else {
         setLoading(false);
         alert(responseData.message);
